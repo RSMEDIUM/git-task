76ec87e725a13bd08c16f3652958f69ecc9f13da
Ryan <ryansandlin01@gmail.com>
2026-01-07 21:44:46 -0600

    Add conversion.py Python script

---
+++ example-repo/conversion.py
@@ -0,0 +1,28 @@
+"""
+I retract my comment on the previous code block on the other assignment. 
+This program will sort variables.
+
+Pseudocode:
+1. convert variables
+2. print out all varaibles on seperate lines
+
+"""
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+#here is where we wil convert the strings into integers, floats, strings, and integers
+
+num1 = int(num1)
+num2 = float(num2)
+num3 = str(num3)
+string1 = int(string1)
+
+#I will print the variables and their types to show that they have been converted
+
+
+print(f"num1: {num1}, type: {type(num1)}")
+print(f"num2: {num2}, type: {type(num2)}")
+print(f"num3: {num3}, type: {type(num3)}")
+print(f"string1; {string1}, type: {type(string1)}")
